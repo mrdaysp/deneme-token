d3b5b0b8d7258b5c8311f5c3bcc55079a83fedaa
mrdaysp <omer.ozen5@gmail.com>
2025-01-29 21:01:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,17 @@
-// app.js
 let web3;
 let userAddress;
-const contractAddress = "0xe3f43cb7a6a090359724f27baee13404ceb7d7ae"; // Kontrat adresi
-const contractABI = [
+let tokenContract;
+let saleContract;
+
+// Kontrat Adresleri (Testnet için güncelleyin)
+const TOKEN_ADDRESS = "0xB9F682e4Fb701855956F3118521c61666c52a9a3";
+const SALE_ADDRESS = "0xd9145CCE52D386f254917e481eB44e9943F39138";
+const PROJECT_WALLET = "0xc98389a82505bA6cb4e9E11965A26cb74baB5cca";
+
+// ABI'ler
+const tokenABI = [
 	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "spender",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "approve",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "tokenAmount",
-				"type": "uint256"
-			}
-		],
-		"name": "buyTokens",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_projectWallet",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "_initialBnbUsdRate",
-				"type": "uint256"
-			},
-			{
-				"internalType": "address",
-				"name": "_initialOwner",
-				"type": "address"
-			}
-		],
+		"inputs": [],
 		"stateMutability": "nonpayable",
 		"type": "constructor"
 	},
@@ -198,19 +152,6 @@ const contractABI = [
 		"anonymous": false,
 		"inputs": [
 			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "newRate",
-				"type": "uint256"
-			}
-		],
-		"name": "BnbUsdRateUpdated",
-		"type": "event"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
 				"indexed": true,
 				"internalType": "address",
 				"name": "previousOwner",
@@ -225,75 +166,6 @@ const contractABI = [
 		],
 		"name": "OwnershipTransferred",
 		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "renounceOwnership",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "_projectWallet",
-				"type": "address"
-			}
-		],
-		"name": "setProjectWallet",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": true,
-				"internalType": "address",
-				"name": "buyer",
-				"type": "address"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "bnbAmount",
-				"type": "uint256"
-			},
-			{
-				"indexed": false,
-				"internalType": "uint256",
-				"name": "tokenAmount",
-				"type": "uint256"
-			}
-		],
-		"name": "TokensPurchased",
-		"type": "event"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "to",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "value",
-				"type": "uint256"
-			}
-		],
-		"name": "transfer",
-		"outputs": [
-			{
-				"internalType": "bool",
-				"name": "",
-				"type": "bool"
-			}
-		],
-		"stateMutability": "nonpayable",
-		"type": "function"
 	},
 	{
 		"anonymous": false,
@@ -319,6 +191,182 @@ const contractABI = [
 		],
 		"name": "Transfer",
 		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "uint256",
+				"name": "_amount",
+				"type": "uint256"
+			}
+		],
+		"name": "burnTokens",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"internalType": "uint8",
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"internalType": "uint256",
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"internalType": "bool",
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
 	},
 	{
 		"inputs": [
@@ -361,16 +409,25 @@ const contractABI = [
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
-	},
+	}
+];
+const saleABI = [
 	{
 		"inputs": [
 			{
 				"internalType": "uint256",
-				"name": "_newRate",
+				"name": "tokenAmount",
 				"type": "uint256"
 			}
 		],
-		"name": "updateBnbUsdRate",
+		"name": "buyTokens",
+		"outputs": [],
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "renounceOwnership",
 		"outputs": [],
 		"stateMutability": "nonpayable",
 		"type": "function"
@@ -379,92 +436,71 @@ const contractABI = [
 		"inputs": [
 			{
 				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"internalType": "address",
-				"name": "spender",
+				"name": "newOwner",
 				"type": "address"
 			}
 		],
-		"name": "allowance",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
+		"name": "transferOwnership",
+		"outputs": [],
+		"stateMutability": "nonpayable",
 		"type": "function"
 	},
 	{
 		"inputs": [
 			{
 				"internalType": "address",
-				"name": "account",
+				"name": "_tokenAddress",
+				"type": "address"
+			},
+			{
+				"internalType": "address",
+				"name": "_projectWallet",
 				"type": "address"
 			}
 		],
-		"name": "balanceOf",
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "previousOwner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"internalType": "address",
+				"name": "newOwner",
+				"type": "address"
+			}
+		],
+		"name": "OwnershipTransferred",
+		"type": "event"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_newWallet",
+				"type": "address"
+			}
+		],
+		"name": "setProjectWallet",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "feePercent",
 		"outputs": [
 			{
 				"internalType": "uint256",
 				"name": "",
 				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "bnbUsdRate",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "decimals",
-		"outputs": [
-			{
-				"internalType": "uint8",
-				"name": "",
-				"type": "uint8"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "FEE_PERCENT",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "name",
-		"outputs": [
-			{
-				"internalType": "string",
-				"name": "",
-				"type": "string"
 			}
 		],
 		"stateMutability": "view",
@@ -485,19 +521,6 @@ const contractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "pricePerTokenInWei",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
 		"name": "projectWallet",
 		"outputs": [
 			{
@@ -511,12 +534,12 @@ const contractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "symbol",
+		"name": "token",
 		"outputs": [
 			{
-				"internalType": "string",
+				"internalType": "contract IERC20",
 				"name": "",
-				"type": "string"
+				"type": "address"
 			}
 		],
 		"stateMutability": "view",
@@ -524,7 +547,7 @@ const contractABI = [
 	},
 	{
 		"inputs": [],
-		"name": "totalSupply",
+		"name": "tokenPriceUSD",
 		"outputs": [
 			{
 				"internalType": "uint256",
@@ -535,52 +558,70 @@ const contractABI = [
 		"stateMutability": "view",
 		"type": "function"
 	}
-]; // Kontrat ABI'si
+];
 
-async function connectWallet() {
+document.getElementById('connectWallet').addEventListener('click', async () => {
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
         try {
             await window.ethereum.request({ method: 'eth_requestAccounts' });
             const accounts = await web3.eth.getAccounts();
             userAddress = accounts[0];
-            updateWalletInfo();
+            
+            // Cüzdan bilgilerini göster
+            document.getElementById('walletInfo').style.display = 'block';
+            document.getElementById('address').textContent = userAddress;
+            
+            // Bakiyeleri güncelle
+            updateBalances();
+            
+            // Satın alma bölümünü aç
+            document.getElementById('buySection').style.display = 'block';
         } catch (error) {
-            console.error("Cüzdan bağlantı hatası:", error);
+            console.error("Cüzdan bağlanamadı:", error);
         }
     } else {
         alert("Lütfen MetaMask yükleyin!");
     }
+});
+
+async function updateBalances() {
+    // tBNB Bakiyesi
+    const bnbBalance = await web3.eth.getBalance(userAddress);
+    document.getElementById('bnbBalance').textContent = web3.utils.fromWei(bnbBalance, 'ether') + " tBNB";
+    
+    // FEN Bakiyesi
+    tokenContract = new web3.eth.Contract(tokenABI, TOKEN_ADDRESS);
+    const tokenBalance = await tokenContract.methods.balanceOf(userAddress).call();
+    document.getElementById('tokenBalance').textContent = web3.utils.fromWei(tokenBalance, 'ether') + " FEN";
 }
 
-async function updateWalletInfo() {
-    const balance = await web3.eth.getBalance(userAddress);
-    document.getElementById('walletInfo').innerHTML = `
-        <p>Adres: ${userAddress}</p>
-        <p>tBNB Bakiye: ${web3.utils.fromWei(balance, 'ether')}</p>
-    `;
+function calculateBNB() {
+    const tokenAmount = document.getElementById('tokenAmount').value;
+    const fee = tokenAmount * 0.03;
+    const totalTokens = parseFloat(tokenAmount) + fee;
+    
+    // Sabit BNB/USD fiyatı (1 BNB = 300$)
+    const requiredBNB = (totalTokens * 0.01) / 300; 
+    document.getElementById('requiredBNB').textContent = requiredBNB.toFixed(6) + " tBNB";
 }
 
 async function buyTokens() {
     const tokenAmount = document.getElementById('tokenAmount').value;
-    const contract = new web3.eth.Contract(contractABI, contractAddress);
-    const price = await contract.methods.pricePerToken().call();
-    const requiredBnb = (tokenAmount * price * 103) / 100;
-
-    contract.methods.buyTokens(tokenAmount).send({
+    saleContract = new web3.eth.Contract(saleABI, SALE_ADDRESS);
+    
+    // BNB hesaplamasını tekrar yap
+    const fee = tokenAmount * 0.03;
+    const totalTokens = parseFloat(tokenAmount) + fee;
+    const requiredBNB = (totalTokens * 0.01) / 300;
+    
+    // Kontratı çağır
+    await saleContract.methods.buyTokens(web3.utils.toWei(tokenAmount, 'ether')).send({
         from: userAddress,
-        value: requiredBnb
-    })
-    .on('transactionHash', (hash) => {
-        document.getElementById('transactionStatus').innerHTML = `İşlem hash: ${hash}`;
-    })
-    .on('receipt', (receipt) => {
-        document.getElementById('transactionStatus').innerHTML = "Başarılı!";
-    })
-    .on('error', (error) => {
-        document.getElementById('transactionStatus').innerHTML = "Hata: " + error.message;
+        value: web3.utils.toWei(requiredBNB.toString(), 'ether'),
+        gas: 300000
     });
+    
+    alert("Satın alma başarılı!");
+    updateBalances();
 }
-
-document.getElementById('connectButton').addEventListener('click', connectWallet);
-document.getElementById('buyButton').addEventListener('click', buyTokens);
